2e9cd8b32ab3da20a210a7c1ea3c5bbd3f647c06
Nadhir Boukhenifra <NadhirBoukhenifra@gmail.com>
2024-01-03 16:44:33 +0100

    Install vuetify-nuxt-module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    // vuetify module
+    'vuetify-nuxt-module'
+  ],
+  // vuetify module config
+  vuetify: {
+    // module specific options
+    moduleOptions: {
+      /*  */
+    },
+    // vuetify options
+    vuetifyOptions: {
+    }
+  }
 })
